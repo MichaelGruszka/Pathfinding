9f8d25d6d09c98854fa6672c3a37b89a5dd6849e
MGruszka <michal.gruszka01@gmail.com>
2024-05-19 10:31:40 +0200

    feature(pathfinding) remove redundant nativearray allocations

--- Assets/_Scripts/Pathfinding/PathfindingJob.cs
+++ Assets/_Scripts/Pathfinding/PathfindingJob.cs
@@ -16,7 +16,6 @@ namespace PathFinding
         public NativeList<int2> Path;
         public NativeArray<bool> OpenSet;
         public NativeArray<bool> ClosedSet;
-
         private int GetIndex(int2 pos)
         {
             return pos.y * GridSize.x + pos.x;
@@ -56,31 +55,34 @@ namespace PathFinding
 
                 OpenSet[currentIndex] = false;
                 ClosedSet[currentIndex] = true;
-
-                NativeArray<int2> neighbors = GetNeighbors(currentPos);
-
-                foreach (int2 neighbor in neighbors)
-                {
-                    int neighborIndex = GetIndex(neighbor);
-                    if (ClosedSet[neighborIndex] || Grid[neighborIndex].Obstacle)
-                        continue;
-
-                    float tentativeGScore = Grid[currentIndex].gScore + SquaredDistance(currentPos, neighbor);
-
-                    if (!OpenSet[neighborIndex] || tentativeGScore < Grid[neighborIndex].gScore)
-                    {
-                        var neighborGridCell = Grid[neighborIndex];
-                        neighborGridCell.cameFromIndex = currentIndex;
-                        neighborGridCell.gScore = tentativeGScore;
-                        neighborGridCell.fScore = tentativeGScore + SquaredDistance(neighbor, End);
-                        Grid[neighborIndex] = neighborGridCell;
-                        OpenSet[neighborIndex] = true;
-                    }
-                }
-                neighbors.Dispose();
+                GetNeighbors(currentPos, out var neighbor1, out var neighbor2, out var neighbor3, out var neighbor4);
+                EvaluateNeighbor(neighbor1, currentIndex, currentPos);
+                EvaluateNeighbor(neighbor2, currentIndex, currentPos);
+                EvaluateNeighbor(neighbor3, currentIndex, currentPos);
+                EvaluateNeighbor(neighbor4, currentIndex, currentPos);
             }
         }
+        private void EvaluateNeighbor(int2 neighborPos, int currentInd, int2 currentPosition)
+        {
+            if (neighborPos.x >= 0)
+            {
+                int neighborIndex = GetIndex(neighborPos);
+                if (ClosedSet[neighborIndex] || Grid[neighborIndex].Obstacle)
+                    return;
 
+                float tentativeGScore = Grid[currentInd].gScore + SquaredDistance(currentPosition, neighborPos);
+
+                if (!OpenSet[neighborIndex] || tentativeGScore < Grid[neighborIndex].gScore)
+                {
+                    var neighborGridCell = Grid[neighborIndex];
+                    neighborGridCell.cameFromIndex = currentInd;
+                    neighborGridCell.gScore = tentativeGScore;
+                    neighborGridCell.fScore = tentativeGScore + SquaredDistance(neighborPos, End);
+                    Grid[neighborIndex] = neighborGridCell;
+                    OpenSet[neighborIndex] = true;
+                }
+            }
+        }
         private static float SquaredDistance(int2 a, int2 b)
         {
             int dx = a.x - b.x;
@@ -123,14 +125,17 @@ namespace PathFinding
             return new int2(x, y);
         }
 
-        private NativeArray<int2> GetNeighbors(int2 pos)
+        private void GetNeighbors(int2 pos, out int2 neighbor1, out int2 neighbor2, out int2 neighbor3, out int2 neighbor4)
         {
-            NativeList<int2> neighbors = new NativeList<int2>(Allocator.TempJob);
-            if (pos.x > 0) neighbors.Add(new int2(pos.x - 1, pos.y));
-            if (pos.x < GridSize.x - 1) neighbors.Add(new int2(pos.x + 1, pos.y));
-            if (pos.y > 0) neighbors.Add(new int2(pos.x, pos.y - 1));
-            if (pos.y < GridSize.y - 1) neighbors.Add(new int2(pos.x, pos.y + 1));
-            return neighbors;
+            neighbor1 = new int2(-1, -1);
+            neighbor2 = new int2(-1, -1);
+            neighbor3 = new int2(-1, -1);
+            neighbor4 = new int2(-1, -1);
+
+            if (pos.x > 0) neighbor1 = (new int2(pos.x - 1, pos.y));
+            if (pos.x < GridSize.x - 1) neighbor2 = (new int2(pos.x + 1, pos.y));
+            if (pos.y > 0) neighbor3 = (new int2(pos.x, pos.y - 1));
+            if (pos.y < GridSize.y - 1) neighbor4 = (new int2(pos.x, pos.y + 1));
         }
 
         private void ReconstructPath(int currentIndex)
